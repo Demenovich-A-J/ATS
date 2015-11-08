910378cd2f78310b48628f8f04548eb443fa35db
Metiska <demenoff.aj@gmail.com>
2015-11-08 15:33:49 +0300

    Added class responce change callinfo fields.

--- ATS/CallInfo.cs
+++ ATS/CallInfo.cs
@@ -6,20 +6,18 @@ namespace ATS
     {
         private readonly PhoneNumber _target;
         private readonly PhoneNumber _source;
-        private readonly DateTime _timeBegin;
 
-        public CallInfo(PhoneNumber target, PhoneNumber source, DateTime timeBegin)
+        public CallInfo(PhoneNumber target, PhoneNumber source)
         {
             _target = target;
             _source = source;
-            _timeBegin = timeBegin;
         }
 
         public PhoneNumber Target => _target;
 
         public PhoneNumber Source => _source;
 
-        public DateTime TimeBegin => _timeBegin;
+        public DateTime TimeBegin { get; set; }
 
         public TimeSpan Duration { get; set; }
     }
---
+++ ATS/Responce.cs
@@ -0,0 +1,16 @@
+﻿using ATS.Station_Model.States;
+
+namespace ATS
+{
+    public class Responce
+    {
+        public Responce(ResponseState state, PhoneNumber source)
+        {
+            State = state;
+            Source = source;
+        }
+
+        public ResponseState State { get; }
+        public PhoneNumber Source { get; }
+    }
+}
