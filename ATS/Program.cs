77932a63663b7b4b7d9802bea2a42f0893fb8c66
Metiska <demenoff.aj@gmail.com>
2015-11-08 15:32:54 +0300

    no message

--- ATS/Program.cs
+++ ATS/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using ATS.Station_Model.Intarfaces;
 using ATS.Station_Model.States;
 using ATS.TestAts;
@@ -9,6 +10,25 @@ namespace ATS
     {
         private static void Main(string[] args)
         {
+            var ports = new List<IPort>() { new TestPort(), new TestPort() };
+            var ats = new Ats(ports,new List<ITerminal>());
+
+            ats.Add(new TestTerminal(new PhoneNumber("000-00-0")));
+            ats.Add(new TestTerminal(new PhoneNumber("111-11-1")));
+
+            foreach (var v in ats.TerminalCollection)
+            {
+                v.Plug();
+                v.Unplug();
+            }
+
+            /*ats.TerminalCollection.ElementAt(0).Call(new PhoneNumber("111-11-1"));
+
+            ats.WaitActionTerminals.ElementAt(0).Answer();*/
+
+            ats.PortCollection.ElementAt(0).State = PortState.Unpluged;
+            ats.PortCollection.ElementAt(0).State = PortState.Free;
+
 
         }
     }
