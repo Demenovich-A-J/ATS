0c3e89bc5320e6c8b5812ebef2bd37b8f8a3832b
Metiska <demenoff.aj@gmail.com>
2015-11-14 00:09:01 +0300

    Control Panel need to correct receiving Terminal, and associate events station and billing system.

---
+++ ATS/Test/ControlPanel.cs
@@ -0,0 +1,20 @@
+﻿using ATS.BilingSystemModel.AbstractClass;
+using ATS.Station_Model.AbstractClasses;
+using ATS.TestAts;
+
+namespace ATS.Test
+{
+    public class ControlPanel
+    {
+
+        private readonly Station _ats;
+        private readonly BilingSystem _bilingSystem;
+
+        public ControlPanel(Station ats, BilingSystem bilingSystem)
+        {
+            _ats = ats;
+            _bilingSystem = bilingSystem;
+        }
+
+    }
+}
