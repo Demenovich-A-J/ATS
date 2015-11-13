3e197314d465ac33b6afde7a3bb36a6a43ba6062
Metiska <demenoff.aj@gmail.com>
2015-11-14 00:00:41 +0300

    no message

--- ATS/Program.cs
+++ ATS/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using ATS.BilingSystemModel.Intarfaces;
 using ATS.Station_Model.Intarfaces;
 using ATS.Station_Model.States;
+using ATS.Test;
 using ATS.TestAts;
+using ATS.User_Model;
 
 namespace ATS
 {
@@ -12,6 +15,9 @@ namespace ATS
         {
             var ports = new List<IPort>() { new TestPort(), new TestPort() };
             var ats = new Ats(ports,new List<ITerminal>());
+            var velcome = new Velcome(new List<ITariffPlan>(),new Dictionary<ITerminal, IUser>());
+
+            ats.CallInfoPrepared += velcome.CallInfoHandler;
 
             ats.Add(new TestTerminal(new PhoneNumber("000-00-0")));
             ats.Add(new TestTerminal(new PhoneNumber("111-11-1")));
