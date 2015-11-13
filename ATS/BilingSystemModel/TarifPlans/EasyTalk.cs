208d86c9f32e523fd2dbcab63dc24c3ad51e4d19
Metiska <demenoff.aj@gmail.com>
2015-11-14 00:06:12 +0300

    Implement new tariflan.

---
+++ ATS/BilingSystemModel/TarifPlans/EasyTalk.cs
@@ -0,0 +1,16 @@
+﻿using ATS.BilingSystemModel.Intarfaces;
+
+namespace ATS.BilingSystemModel.TarifPlans
+{
+    public class EasyTalk : ITariffPlan
+    {
+        public EasyTalk(double costOneCall, int freeMinutes)
+        {
+            CostOneMinute = costOneCall;
+            FreeMinutes = freeMinutes;
+        }
+
+        public double CostOneMinute { get; }
+        public double FreeMinutes { get; set; }
+    }
+}
