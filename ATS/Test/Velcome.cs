3ba4b884f08f0a8f0acf0397b959db04fa5ed492
Metiska <demenoff.aj@gmail.com>
2015-11-14 00:09:21 +0300

    Implement new billing system.

---
+++ ATS/Test/Velcome.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using ATS.BilingSystemModel.AbstractClass;
+using ATS.BilingSystemModel.Intarfaces;
+using ATS.Station_Model.Intarfaces;
+using ATS.User_Model;
+
+namespace ATS.Test
+{
+    public class Velcome : BilingSystem
+    {
+        public Velcome(ICollection<ITariffPlan> tariffPlans, IDictionary<ITerminal, IUser> terminalsUserMapp) : base(tariffPlans, terminalsUserMapp)
+        {
+        }
+    }
+}
