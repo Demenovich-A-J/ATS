63d42e750cc2dd75478d30b4b8bc9058e08af98f
Metiska <demenoff.aj@gmail.com>
2015-11-14 17:00:59 +0300

    added time helper to speed up time in programm

---
+++ ATS/Helpers/TimeHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace ATS.Helpers
+{
+    public static class TimeHelper
+    {
+        private static int _counter = 0;
+
+        public static DateTime Now
+        {
+            get
+            {
+                _counter++;
+                return DateTime.Now.Add(new TimeSpan(5 * _counter, 0, 10 * _counter, 5 * _counter));
+            }
+        }
+
+        public static TimeSpan Duration()
+        {
+            return new TimeSpan(0, new Random().Next(0, 59), new Random().Next(0, 59));
+        }
+    }
+}
