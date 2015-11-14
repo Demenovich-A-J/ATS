51c174c961297777033ef44c27a28b687088e1af
Metiska <demenoff.aj@gmail.com>
2015-11-14 11:56:57 +0300

    no message

--- ATS/Program.cs
+++ ATS/Program.cs
@@ -1,41 +1,36 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using ATS.BillingSystemModel.Intarfaces;
+using ATS.BillingSystemModel.TarifPlans;
 using ATS.Station_Model.Intarfaces;
-using ATS.Station_Model.States;
 using ATS.Test;
 using ATS.TestAts;
 using ATS.User_Model;
 
 namespace ATS
 {
-    internal class Program
+    internal static class Program
     {
         private static void Main(string[] args)
         {
-            var ports = new List<IPort>() { new TestPort(), new TestPort() };
-            var ats = new Ats(ports,new List<ITerminal>());
-            var velcome = new Velcome(new List<ITariffPlan>(),new Dictionary<ITerminal, IUser>());
+            var ats = new Ats(new List<IPort>(), new List<ITerminal>());
+            var velcome = new Velcome(new List<ITariffPlan>());
 
-            ats.CallInfoPrepared += velcome.CallInfoHandler;
+            var panel = new ControlPanel(ats,velcome);
 
-            ats.Add(new TestTerminal(new PhoneNumber("000-00-0")));
-            ats.Add(new TestTerminal(new PhoneNumber("111-11-1")));
-
-            foreach (var v in ats.TerminalCollection)
-            {
-                v.Plug();
-            }
-
-            ats.TerminalCollection.ElementAt(0).Call(new PhoneNumber("111-11-1"));
-
-            ats.TerminalCollection.ElementAt(1).Answer();
-
-            ats.TerminalCollection.ElementAt(1).Drop();
+            var user1 = new User();
+            var user2 = new User();
 
 
-            //ats.PortCollection.ElementAt(0).State = PortState.Unpluged;
-            //ats.PortCollection.ElementAt(0).State = PortState.Free;
+            user1.Phone = panel.GetContracTerminal(user1, new EasyTalk(25.1,0));
+            user2.Phone = panel.GetContracTerminal(user2, new EasyTalk(25.1, 0));
+
+            user1.Plug();
+            user2.Plug();
+
+            user1.Call(user2.Phone.Number);
+            user2.Answer();
+            user2.Drop();
         }
     }
 }
