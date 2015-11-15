208d9ff991a3555649f22e23c33cbec301cbe4ee
Metiska <demenoff.aj@gmail.com>
2015-11-15 17:34:48 +0300

    added new date handler, need to disconect user.

--- ATS/Test/ControlPanel.cs
+++ ATS/Test/ControlPanel.cs
@@ -1,7 +1,12 @@
-﻿using ATS.BillingSystemModel.AbstractClass;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ATS.BillingSystemModel.AbstractClass;
 using ATS.BillingSystemModel.Intarfaces;
+using ATS.Helpers;
 using ATS.Station_Model.AbstractClasses;
 using ATS.Station_Model.Intarfaces;
+using ATS.Station_Model.States;
 using ATS.User_Model;
 
 namespace ATS.Test
@@ -11,12 +16,14 @@ namespace ATS.Test
 
         private readonly Station _ats;
         private readonly BillingSystem _bilingSystem;
-
+        private readonly ICollection<IUser> _disconectedUserCollection; 
         public ControlPanel(Station ats, BillingSystem bilingSystem)
         {
             _ats = ats;
             _bilingSystem = bilingSystem;
+            _disconectedUserCollection = new List<IUser>();
             _ats.CallInfoPrepared += _bilingSystem.CallInfoHandler;
+            TimeHelper.NewDateTime += DateChangeHandler;
         }
 
         public ITerminal GetContracTerminal(IUser user,ITariffPlan tariffPlan)
@@ -34,5 +41,19 @@ namespace ATS.Test
             return terminal;
         }
 
+        private void DateChangeHandler(object sender, DateTime time)
+        {
+            foreach (var info in _bilingSystem.UserDateTimesMapp.Where(info => info.Value.AddMonths(2) <= time).Where(info => !_disconectedUserCollection.Contains(info.Key)))
+            {
+                info.Key.Drop();
+                info.Key.Phone.ClearEvents();
+
+                Console.WriteLine("|=====================================================|");
+                _ats.PortsMapping[info.Key.Phone.Number].State = PortState.Unpluged;
+                _disconectedUserCollection.Add(info.Key);
+                Console.WriteLine($"Abonent : {info.Key.Phone.Number.Number}; Was disconect from station. Because of non-payment;");
+                Console.WriteLine("|=====================================================|");
+            }
+        }
     }
 }
