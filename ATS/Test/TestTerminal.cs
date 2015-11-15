90361d51b8d660f660524c8f412ab0e1ac56ab0f
Metiska <demenoff.aj@gmail.com>
2015-11-15 17:38:59 +0300

    override some methods added conditions to correct work

--- ATS/Test/TestTerminal.cs
+++ ATS/Test/TestTerminal.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using ATS.BillingSystemModel.Intarfaces;
 using ATS.Station_Model.AbstractClasses;
+using ATS.Station_Model.States;
 
 namespace ATS.Test
 {
     public class TestTerminal : Terminal
     {
-        
 
+        public TerminalState State { get; set; }
         private void OnIncomingRequest(object sender, PhoneNumber source)
         {
             Console.WriteLine("{0} received request for incoming connection from {1}", Number.Number, source.Number);
@@ -15,9 +16,40 @@ namespace ATS.Test
 
         public TestTerminal(PhoneNumber number, ITariffPlan tariffPlan) : base(number, tariffPlan)
         {
+            State = TerminalState.Free;
             IncomingRequest += OnIncomingRequest;
+            OutgoingCall += (sender, info) => { Console.WriteLine($"Phone : {((Terminal)sender).Number.Number} call to {info.Target.Number}"); };
             Online += (sender, args) => { Console.WriteLine($"Phone {((Terminal)sender).Number.Number} now Online"); };
             Offline += (sender, args) => { Console.WriteLine($"Phone {((Terminal)sender).Number.Number} now offline"); };
+        }
+
+        public override void Drop()
+        {
+            if(State == TerminalState.Free) return;
+            base.Drop();
+        }
+
+        public override void Call(PhoneNumber target)
+        {
+            base.Call(target);
+            State = TerminalState.OutGoingCall;
+        }
+
+        public override void Answer()
+        {
+            if(State != TerminalState.IncomingCall) return;
+            base.Answer();
+        }
+
+        public override void Reject()
+        {
+            if (State != TerminalState.IncomingCall) return;
+            base.Reject();
+        }
+        protected override void OnIncomingRequest(PhoneNumber source)
+        {
+            base.OnIncomingRequest(source);
+            State = TerminalState.IncomingCall;
         }
     }
 }
